b873c5f241a1d43fda6aa46860606fbf0966af5a
TaWeiTu <tu.da.wei@gmail.com>
2021-03-11 11:33:23 +0800

    Successfully send ICMP packets

--- traceroute.cpp
+++ traceroute.cpp
@@ -4,6 +4,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#include <array>
+#include <chrono>
 #include <iostream>
 #include <utility>
 
@@ -45,19 +47,61 @@ std::pair<Mode, char *> ParseArg(int argc, char *argv[]) {
   return std::make_pair(mode, argv[optind]);
 }
 
+struct ICMPPacket {
+  static constexpr uint8_t kEchoReply = 0x0;
+  static constexpr uint8_t kEchoRequest = 0x8;
+
+  static constexpr int kProtocol = 1;
+
+  // type + code + checksum + identifier + seq
+  static constexpr size_t kPacketSize = 8;
+
+  uint8_t type;
+  uint8_t code;
+  uint16_t checksum;
+  uint16_t identifier;
+  uint16_t sequence_number;
+
+  ICMPPacket(uint16_t identifier, uint16_t sequence_number)
+      : type(kEchoRequest),
+        code(0x0),
+        identifier(htons(identifier)),
+        sequence_number(htons(sequence_number)) {
+    uint32_t sum = (static_cast<uint32_t>(type) << 8) + code + identifier +
+                   sequence_number;
+    uint16_t high = static_cast<uint16_t>(sum >> 16);
+    uint16_t low = static_cast<uint16_t>(sum & ((1U << 16) - 1));
+    checksum = htons(~(high + low));
+  }
+};
+
 int main(int argc, char *argv[]) {
   auto [mode, host] = ParseArg(argc, argv);
 
   struct sockaddr_in addr;
-  addr.sin_port = htons(7);
+  addr.sin_port = htons(7122);
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = LookUp(host);
 
-  constexpr int kMaxHop = 64;
+  constexpr int kMaxHop = 1;
   std::cout << "traceroute to " << host << " (" << inet_ntoa(addr.sin_addr)
             << "), " << kMaxHop << " hops max\n";
 
-  for (int hop = 1; hop < kMaxHop; ++hop) {
+  for (int hop = 0; hop < kMaxHop; ++hop) {
+    // TODO: properly set up identifer and sequence number
+    ICMPPacket packet(0x7122, 0x1234);
+    int fd = socket(AF_INET, SOCK_RAW, ICMPPacket::kProtocol);
+    if (fd == -1) {
+      perror("socket");
+      exit(1);
+    }
+    ssize_t sz =
+        sendto(fd, reinterpret_cast<const void *>(&packet), sizeof(packet), 0,
+               reinterpret_cast<const struct sockaddr *>(&addr), sizeof(addr));
+    if (sz == -1) {
+      perror("sendto");
+      exit(1);
+    }
   }
   return 0;
 }
